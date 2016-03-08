ce756549335a4f8fe575756499f703250857d071
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-03-08 17:43:36 +0700

    add menu

--- www/js/controllers/member.js
+++ www/js/controllers/member.js
@@ -1,9 +1,19 @@
-myApp.controller('memberController', ['$scope', '$location', function($scope, $location) {
+myApp.controller('memberController', ['$scope', '$location', '$ionicPopover', function($scope, $location, $ionicPopover) {
 
 
     $scope.var = 1;
     $scope.click = function(num) {
         $scope.var = num;
     }
+
+    $scope.animation = 'slide-in-up';
+
+    $ionicPopover.fromTemplateUrl('templates/menu.html', {
+        scope: $scope,
+        animation: $scope.animation
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
+
 
 }]);
